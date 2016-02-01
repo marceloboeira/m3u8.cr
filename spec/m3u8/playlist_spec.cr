bf6eb6e4e752ff3bb28d286a96de4bc74a435c7e
Marcelo Boeira <me@marceloboeira.com>
2016-02-01 11:32:32 -0200

    test: Validate independent parts instead of the whole output

--- spec/m3u8/playlist_spec.cr
+++ spec/m3u8/playlist_spec.cr
@@ -4,25 +4,18 @@ describe M3U8::Playlist do
   describe "when creating a playlist" do
     let(playlist) { M3U8::Playlist.new }
 
-    it "inserts the base header" do
-      output = \
-          "#EXTM3U\n" \
-          "\n"
-
-      expect(playlist.to_s).to eq(output)
+    it "inserts the header" do
+      expect(playlist.to_s).to match(/#EXTM3U\n/)
     end
 
     describe "media playlist" do
       it "accepts new segments" do
         playlist.add_segment("S01E01-1080-0001.ts", 9.003)
 
-        output = \
-          "#EXTM3U\n" \
-          "\n" \
-          "#EXTINF:9.003,\n" \
-          "S01E01-1080-0001.ts\n"
+        output = "#EXTINF:9.003,\n" \
+                 "S01E01-1080-0001.ts\n"
 
-        expect(playlist.to_s).to eq(output)
+        expect(playlist.to_s).to match(/#{output}/)
       end
     end
   end
