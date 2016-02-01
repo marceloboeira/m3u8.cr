833636ad71d7febeb57b3f5b30675874b7ac92a4
Marcelo Boeira <me@marceloboeira.com>
2016-02-01 11:35:03 -0200

    test: Simplify scenarios

--- spec/m3u8/playlist_spec.cr
+++ spec/m3u8/playlist_spec.cr
@@ -1,22 +1,20 @@
 require "./../spec_helper"
 
 describe M3U8::Playlist do
-  describe "when creating a playlist" do
+  context "when creating a playlist" do
     let(playlist) { M3U8::Playlist.new }
 
     it "inserts the header" do
       expect(playlist.to_s).to match(/#EXTM3U\n/)
     end
 
-    describe "media playlist" do
-      it "accepts new segments" do
-        playlist.add_segment("S01E01-1080-0001.ts", 9.003)
+    it "accepts new segments" do
+      playlist.add_segment("S01E01-1080-0001.ts", 9.003)
 
-        output = "#EXTINF:9.003,\n" \
-                 "S01E01-1080-0001.ts\n"
+      output = "#EXTINF:9.003,\n" \
+        "S01E01-1080-0001.ts\n"
 
-        expect(playlist.to_s).to match(/#{output}/)
-      end
+      expect(playlist.to_s).to match(/#{output}/)
     end
   end
 end
