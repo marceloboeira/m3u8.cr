c36441dd84f0f59a9aac3cc4105925bd1db24b35
Marcelo Boeira <me@marceloboeira.com>
2016-02-02 21:42:10 -0200

    feature: Add EXT-X-TARGETDURATION tag support
    Workaround with in the header private method while haven't a better idea

--- spec/m3u8/playlist_spec.cr
+++ spec/m3u8/playlist_spec.cr
@@ -2,14 +2,19 @@ require "./../spec_helper"
 
 describe M3U8::Playlist do
   context "when creating a playlist" do
-    let(playlist) { M3U8::Playlist.new }
+    let(target_duration) { 3 }
+    let(playlist) { M3U8::Playlist.new(target_duration) }
 
     it "inserts the header" do
       expect(playlist.to_s).to match(/#EXTM3U\n/)
     end
 
+    it "inserts the target-duration tag" do
+      expect(playlist.to_s).to match(/#EXT-X-TARGETDURATION:3\n/)
+    end
+
     it "inserts an empty line between the header and rest" do
-      expect(playlist.to_s.lines[1]).to eq("\n")
+      expect(playlist.to_s.lines[2]).to eq("\n")
     end
 
     it "accepts new segments" do
--- src/m3u8/playlist.cr
+++ src/m3u8/playlist.cr
@@ -1,6 +1,6 @@
 module M3U8
   class Playlist
-    def initialize
+    def initialize(@target_duration : Int32)
       @builder = ""
       @builder += header
     end
@@ -30,7 +30,23 @@ module M3U8
     # It MUST be the first line of every Media Playlist and every Master Playlist
     private def header
       "#EXTM3U\n" \
+      "#{target_duration}" \
       "\n"
+    end
+
+    # EXT-X-TARGETDURATION
+    # https://tools.ietf.org/html/draft-pantos-http-live-streaming-18#section-4.3.3.1
+    #
+    # The EXT-X-TARGETDURATION tag specifies the maximum Media Segment
+    # duration. The EXTINF duration of each Media Segment in the Playlist
+    # file, when rounded to the nearest integer, MUST be less than or equal
+    # to the target duration; longer segments can trigger playback stalls
+    # or other errors. It applies to the entire Playlist file.
+    # Its format is: EXT-X-TARGETDURATION:<s>
+    # - s is a decimal-integer indicating the target duration in seconds.
+    # The EXT-X-TARGETDURATION tag is REQUIRED.
+    private def target_duration
+      "#EXT-X-TARGETDURATION:#{@target_duration}\n"
     end
   end
 end
