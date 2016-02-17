8fbabd7280a56eda82c2260727b865903e4a2643
Marcelo Boeira <me@marceloboeira.com>
2016-02-17 07:37:39 -0200

    test: Add scenario with dynamic segments

--- spec/m3u8/playlist_spec.cr
+++ spec/m3u8/playlist_spec.cr
@@ -3,7 +3,12 @@ require "./../spec_helper"
 describe M3U8::Playlist do
   context "when rendering a playlist" do
     let(target_duration) { 3 }
-    let(playlist) { M3U8::Playlist.new(target_duration) }
+    let(:segments) {
+      [ M3U8::Playlist::Segment.new("S01E01-1080-0001.ts", 9.003),
+        M3U8::Playlist::Segment.new("S01E01-1080-0002.ts", 9.003),
+        M3U8::Playlist::Segment.new("S01E01-1080-0003.ts", 9.003) ]
+    }
+    let(playlist) { M3U8::Playlist.new(target_duration, segments) }
 
     it "inserts the header" do
       expect(playlist.to_s).to match(/#EXTM3U\n/)
@@ -22,20 +27,17 @@ describe M3U8::Playlist do
     end
 
     context "with segments" do
-      let(:segments) {
-        [ M3U8::Playlist::Segment.new("S01E01-1080-0001.ts", 9.003),
-          M3U8::Playlist::Segment.new("S01E01-1080-0002.ts", 9.003),
-          M3U8::Playlist::Segment.new("S01E01-1080-0003.ts", 9.003) ]
-      }
-
-      before do
-        playlist.segments = segments
-      end
-
       it "renders multiple segments" do
         segments.each do |segment|
           expect(playlist.to_s).to match(/#{segment.to_s}/)
         end
+      end
+
+      it "allows to add segments on the fly" do
+        new_segment = M3U8::Playlist::Segment.new("S01E01-1080-0004.ts", 9.003)
+        playlist.segments << new_segment
+
+        expect(playlist.to_s).to match(/#{new_segment.to_s}/)
       end
     end
   end
