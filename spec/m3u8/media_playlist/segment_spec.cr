78ad61d7d84ea7586646b0f366903c4d446c0d27
Marcelo Boeira <me@marceloboeira.com>
2016-02-17 21:46:24 -0200

    refactor: Use default values instead of method overloading

--- spec/m3u8/media_playlist/segment_spec.cr
+++ spec/m3u8/media_playlist/segment_spec.cr
@@ -6,7 +6,7 @@ describe M3U8::MediaPlaylist::Segment do
       segment = M3U8::MediaPlaylist::Segment.new("S01E01-1080-0001.ts", 9.003)
 
       output = "#EXTINF:9.003,\n" \
-        "S01E01-1080-0001.ts\n"
+               "S01E01-1080-0001.ts\n"
 
       expect(segment.to_s).to match(/#{output}/)
     end
@@ -15,7 +15,7 @@ describe M3U8::MediaPlaylist::Segment do
       segment = M3U8::MediaPlaylist::Segment.new("S01E02-1080-0002.ts", 9.009, "LOST - S01E02")
 
       output = "#EXTINF:9.009,LOST - S01E02\n" \
-        "S01E02-1080-0002.ts\n"
+               "S01E02-1080-0002.ts\n"
 
       expect(segment.to_s).to match(/#{output}/)
     end
